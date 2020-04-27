1b418383851f79eee2988e12b7e589ecbd5c1c76
Flavio Francisco <flaviofrancis@msn.com>
2020-04-27 11:22:39 +0200

    Code re-factored/ Check country

--- stackoverflow/webscraping.py
+++ stackoverflow/webscraping.py
@@ -6,14 +6,18 @@ import operator
 from lxml.html import fromstring
 
 _skills = [] # empty list
+_place_name = None
+_usa_states = ["AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UM", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"]
+_canada_provinces = ["AB", "BC", "MB", "NB", "NL", "NT", "NS", "NU", "ON", "PE", "QC", "SK", "YT"]
 
 def main(argv):
     
-    place_name = None
-    pages = 10
+    global _place_name
+
+    pages = 1
 
     if len(sys.argv) >= 2:
-        place_name = sys.argv[1] 
+        _place_name = sys.argv[1] 
 
     if len(sys.argv) >= 3:
         pages = int(sys.argv[2])
@@ -22,15 +26,18 @@ def main(argv):
     is_continue = True
     i = 0
 
+    # Uncomment for debugging...
+    #_place_name = 'Brazil'
+
     while is_continue:
 
         if i == 0:
             i = 1
-            query = {'d': 20, 'l': place_name, 'u': 'Km' }
+            query = {'d': 20, 'l': _place_name, 'u': 'Km' }
         else:
-            query = {'d': 20, 'l': place_name, 'u': 'Km', 'pg': i}
+            query = {'d': 20, 'l': _place_name, 'u': 'Km', 'pg': i}
 
-        if place_name is None:
+        if _place_name is None:
             del query['l']
 
         req = requests.get('https://stackoverflow.com/jobs', params=query)
@@ -46,7 +53,7 @@ def main(argv):
         print(req.url)
         print('Processing page ' + str(i) + ' of ' + str(pages))
 
-    create_file(req.url, place_name)
+    create_file(req.url, _place_name)
 
 
 def get_text(el, class_name):
@@ -56,32 +63,67 @@ def get_text(el, class_name):
     else:
         return ''
 
-
-def get_element(el, class_name):
-    return el.find_class(class_name)
-
-
 def process_page(doc):
     list_result = doc.find_class('listResults')    
-    if list_result:
-        get_results(list_result)
+    if list_result is not None:
+        process_results(list_result)
 
 
-def get_results(list_result):
-    for result in list_result:
-        elements = result.find_class('-job js-result')              
-        if elements and len(elements) > 0:            
-            for el in elements:
-                results = el.find_class('grid')                
-                if results:
-                    for result in results:
-                        skills = get_element(result, 'post-tag no-tag-menu')
-                        if skills:
-                            for skill in skills:
-                                populate_skills(skill.text)
+def process_results(list_result):
+    if list_result is not None:
+        for x in list_result:
+            process_listed_jobs(x)
 
 
-def populate_skills(skill_name):
+def process_listed_jobs(elements):
+    if elements is not None:
+        for el in elements:
+            grids = el.find_class('grid')
+            if grids:
+                process_job_skills(grids)
+
+
+def process_job_skills(elements):
+    if elements is not None:
+        for x in elements:
+            if is_place(x):
+                skills = x.find_class('post-tag no-tag-menu')
+                for skill in skills:
+                    count_skills(skill.text)
+        
+
+def is_place(element):
+    
+    global _place_name
+    global _usa_states
+
+    if _place_name is None:
+        return True
+
+    company_location = element.find_class('fc-black-700 fs-body1 mb4')
+
+    if len(company_location) > 0 and len(company_location[0]) > 1:        
+        city_place = company_location[0][1].text.split(',')
+        if len (city_place) > 1:
+            if is_country('usa', _usa_states, city_place[1].strip()):
+                return True
+            if is_country('canada', _canada_provinces, city_place[1].strip()):
+                return True
+            return city_place[1].strip().lower() == _place_name.lower()
+        else:
+            return city_place[0].strip().lower() == _place_name.lower()
+
+    return False
+
+
+def is_country(country_name, country_states_provinces, value):
+    global _place_name
+    if country_name.lower() == _place_name.lower() and value in country_states_provinces:
+        return True
+    return False
+
+
+def count_skills(skill_name):
     global _skills
     skill = next((x for x in _skills if x.name == skill_name), None)
     if skill is not None:
